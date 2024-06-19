69b3562753126edc823a5185d36e6c14f2a6b6f7
Shyrine Salvador <84778328+shyrxnx@users.noreply.github.com>
2024-06-19 19:24:16 +0800

    fixed comment

--- src/com/graduate/EscapeTheLabyrinth.java
+++ src/com/graduate/EscapeTheLabyrinth.java
@@ -6,6 +6,6 @@ import javax.swing.*;
 
 public class EscapeTheLabyrinth {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(SplashScreen::new); // Show the splash screen
+        SwingUtilities.invokeLater(SplashScreen::new); // Start the game
     }
 }
